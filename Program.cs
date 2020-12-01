3f636131a2c2ce48d3dd1c87e7aec89efe12ee0a
Joseph <josephfinney@live.com>
2020-12-01 12:52:43 -0600

    init with simple .net5

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2020_Advent_Of_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
