9f53a1309628efeace1c4a1687d8d7919b5015de
Joseph <josephfinney@live.com>
2020-12-04 13:13:34 -0600

    Prepare for Day 5

---
+++ Day5.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _2020_Advent_Of_Code
+{
+    public class Day5
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}
