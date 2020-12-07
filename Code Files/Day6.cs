3fcbb90ac0a72ea84d67c545f867e1f006dfca5e
Joseph Finney <TheJoeFin@users.noreply.github.com>
2020-12-07 14:03:05 +0000

    Fix annoying warning message

--- Code Files/Day6.cs
+++ Code Files/Day6.cs
@@ -12,23 +12,23 @@ namespace _2020_Advent_Of_Code
     {
         public HashSet<char> YesAnswers { get; set; } = new HashSet<char>();
         public List<string> ListOfPersonAnswers { get; set; } = new List<string>();
-        public int AllYesAnswers 
-        { 
+        public int AllYesAnswers
+        {
             get
             {
                 int numberOfYes = 0;
-                
+
                 foreach (char yesAns in YesAnswers)
                 {
                     bool YesToAll = true;
 
                     foreach (string personAnswer in ListOfPersonAnswers)
                     {
-                        if(personAnswer.Contains(yesAns) == false)
+                        if (personAnswer.Contains(yesAns) == false)
                             YesToAll = false;
                     }
 
-                    if(YesToAll)
+                    if (YesToAll)
                         numberOfYes++;
                 }
 
@@ -36,23 +36,10 @@ namespace _2020_Advent_Of_Code
             }
         }
     }
-    
+
     public static class Day6
     {
-
-        public static IEnumerable<string> ReadLines(this string s)
-        {
-            string line;
-            using (var sr = new StringReader(s))
-                while ((line = sr.ReadLine()) != null)
-                    yield return line;
-        }
-
-        public static void Run()
-        {
-            string inputString = File.ReadAllText($"Input Files/Day6Input.txt");
-
-            string testString = @"
+        private const string testString = @"
 abc
 
 a
@@ -70,6 +57,17 @@ a
 b
             ";
 
+        public static IEnumerable<string> ReadLines(this string s)
+        {
+            string line;
+            using (var sr = new StringReader(s))
+                while ((line = sr.ReadLine()) != null)
+                    yield return line;
+        }
+
+        public static void Run()
+        {
+            string inputString = File.ReadAllText($"Input Files/Day6Input.txt");
 
             // List<string> inputList = ConvertStringToListOfStrings(inputString);
 
@@ -89,7 +87,7 @@ b
                 // int countYes = uniqueLetters.Count();
                 // Console.WriteLine($"Number of Yes: {countYes} - {customsAnswers}");
 
-                if(cleanLine != string.Empty)
+                if (cleanLine != string.Empty)
                 {
                     newBoardingGroup.ListOfPersonAnswers.Add(cleanLine);
 
@@ -99,7 +97,7 @@ b
                     }
                 }
 
-                if(cleanLine == string.Empty && newBoardingGroup.ListOfPersonAnswers.Count() > 0)
+                if (cleanLine == string.Empty && newBoardingGroup.ListOfPersonAnswers.Count() > 0)
                 {
                     totalCommonYes += newBoardingGroup.AllYesAnswers;
                     EveryoneOnThePlane.Add(newBoardingGroup);
@@ -110,7 +108,7 @@ b
                 // total += countYes;
             }
 
-            if(newBoardingGroup.ListOfPersonAnswers.Count() > 0)
+            if (newBoardingGroup.ListOfPersonAnswers.Count() > 0)
             {
                 totalCommonYes += newBoardingGroup.AllYesAnswers;
             }
