4037fac8f48d5a3e4e6c806287fa508127c2ba84
Joseph Finney <josephfinney@LIVE.COM>
2020-12-09 06:26:57 -0600

    Set up day 8

--- Code Files/Day8.cs
+++ Code Files/Day8.cs
@@ -7,16 +7,15 @@ using System.Reflection;
 
 namespace _2020_Advent_Of_Code
 {
-    public static class DayNUM
+    public static class Day8
     {
         public const string TestString = @"";
 
-
         public static void Run()
         {
-            Console.WriteLine($"Running DayNUM {DateTime.Now.ToShortTimeString()}");
+            Console.WriteLine($"Running Day 8 {DateTime.Now.ToShortTimeString()}");
 
-            string inputString = File.ReadAllText($"Input Files/DayNUMInput.txt");
+            string inputString = File.ReadAllText($"Input Files/Day8Input.txt");
         }
     }
 }
