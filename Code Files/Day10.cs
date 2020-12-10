4673ae1950dc69316dd58dbbf5c249aa38253fe4
Joseph Finney <TheJoeFin@users.noreply.github.com>
2020-12-10 17:02:09 +0000

    added test strings

--- Code Files/Day10.cs
+++ Code Files/Day10.cs
@@ -9,7 +9,49 @@ namespace _2020_Advent_Of_Code
 {
     public static class Day10
     {
-        public const string TestString = @"";
+        public const string TestString = @"16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4";
+
+        public const string TestString2 = @"28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3";
 
         private const string dayNumber = "10";
 
