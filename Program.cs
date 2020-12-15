c0c9fdf915e61b4be0a92ce82dee62f827744a90
Joseph Finney <josephfinney@LIVE.COM>
2020-12-14 19:59:17 -0600

    run day 14 in program

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace _2020_Advent_Of_Code
         static void Main(string[] args)
         {
             // Console.Clear();
-            Day13.Run();
+            Day14.Run();
         }
     }
 }
