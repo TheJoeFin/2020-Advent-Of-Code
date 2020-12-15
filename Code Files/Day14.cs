5b2501034ea17ecc8af368f05cf5bd7a02cda76e
Joseph Finney <josephfinney@LIVE.COM>
2020-12-14 19:58:49 -0600

    layout day 14

--- Code Files/Day14.cs
+++ Code Files/Day14.cs
@@ -22,7 +22,22 @@ namespace _2020_Advent_Of_Code
 
             // Docking Data
 
+            ulong[] memory = new ulong[2 ^ 35];
 
+            ulong[] initMemory = initalizationProgram(memory);
+            ulong sum = sumAllMemory(initMemory);
+
+
+        }
+
+        private static ulong sumAllMemory(ulong[] initMemory)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static ulong[] initalizationProgram(ulong[] memory)
+        {
+            throw new NotImplementedException();
         }
     }
 }
