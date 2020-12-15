29367c95581e3046d2cdb26314abbdb8b53d7b2d
Joseph Finney <josephfinney@LIVE.COM>
2020-12-14 19:38:58 -0600

    Prep for day 14

---
+++ Code Files/Day14.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace _2020_Advent_Of_Code
+{
+    public static class Day14
+    {
+        public const string TestString = @"";
+
+        private const string dayNumber = "14";
+
+
+        public static void Run()
+        {
+            Console.WriteLine($"Running Day {dayNumber} {DateTime.Now.ToShortTimeString()}");
+
+            string inputString = File.ReadAllText($"Input Files/Day{dayNumber}Input.txt");
+
+            // Docking Data
+
+
+        }
+    }
+}
