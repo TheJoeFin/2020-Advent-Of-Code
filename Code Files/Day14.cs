f90b55a785710f35c25a3dd5c5ae7ddc7aedeb3f
Joseph Finney <TheJoeFin@users.noreply.github.com>
2020-12-15 14:23:44 +0000

    Part 1 complete

--- Code Files/Day14.cs
+++ Code Files/Day14.cs
@@ -9,7 +9,10 @@ namespace _2020_Advent_Of_Code
 {
     public static class Day14
     {
-        public const string TestString = @"";
+        public const string TestString = @"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0";
 
         private const string dayNumber = "14";
 
@@ -23,14 +26,16 @@ namespace _2020_Advent_Of_Code
             // Docking Data
 
             Dictionary<ulong, ulong> initMemory = initalizationProgram(inputString);
-            // ulong sum = sumAllMemory(initMemory);
+            ulong sum = sumAllMemory(initMemory);
 
-
+            Console.WriteLine($"The sum of all numbers in memory is {sum}");
 
         }
 
         private static ulong sumAllMemory(Dictionary<ulong, ulong> initMemory)
         {
+            Console.WriteLine("Summing Memory");
+
             ulong runningSum = 0;
 
             foreach (ulong itemInMemory in initMemory.Values)
@@ -43,6 +48,7 @@ namespace _2020_Advent_Of_Code
 
         private static Dictionary<ulong, ulong> initalizationProgram(string bootProgram)
         {
+            Console.WriteLine("Initializing Program");
             Dictionary<ulong, ulong> initedmemory = new Dictionary<ulong, ulong>();
 
             string mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
@@ -65,11 +71,35 @@ namespace _2020_Advent_Of_Code
 
                     ulong maskedNumber = maskNumber(lineNumber, mask);
 
-                    initedmemory.Add(memoryLocation, maskedNumber);
+                    if (initedmemory.ContainsKey(memoryLocation))
+                        initedmemory[memoryLocation] = maskedNumber;
+                    else
+                        initedmemory.Add(memoryLocation, maskedNumber);
                 }
             }
 
             return initedmemory;
+        }
+
+        private static ulong maskNumber(ulong lineNumber, string mask)
+        {
+            ulong returnMaskedNumber = 0;
+
+            char[] lineString = Convert.ToString((int)lineNumber, 2).PadLeft(36, '0').ToArray();
+
+            for (int i = 35; i >= 0; i--)
+            {
+                if (mask[i] == 'X')
+                    continue;
+
+                lineString[i] = mask[i];
+            }
+
+            string binaryString = string.Join("", lineString);
+            // Console.WriteLine(binaryString.ToString());
+            returnMaskedNumber = Convert.ToUInt64(binaryString, 2);
+
+            return returnMaskedNumber;
         }
     }
 }
