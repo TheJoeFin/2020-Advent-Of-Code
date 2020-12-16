b45594121636837ae852cbf53eb52b991b55af7b
Joseph Finney <josephfinney@LIVE.COM>
2020-12-16 06:56:26 -0600

    Finished part 2

--- Code Files/Day14.cs
+++ Code Files/Day14.cs
@@ -14,6 +14,11 @@ mem[8] = 11
 mem[7] = 101
 mem[8] = 0";
 
+        public const string TestString2 = @"mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1";
+
         private const string dayNumber = "14";
 
 
@@ -52,11 +57,13 @@ mem[8] = 0";
             Dictionary<ulong, ulong> initedmemory = new Dictionary<ulong, ulong>();
 
             string mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            int numberOfMaskLoops = 0;
             foreach (string rawLine in bootProgram.ReadLines())
             {
                 if (rawLine.StartsWith("mask = "))
                 {
                     mask = rawLine.Substring(7, 36);
+                    numberOfMaskLoops = countLoopsInMask(mask);
                 }
                 else
                 {
@@ -64,21 +71,92 @@ mem[8] = 0";
                     // int indexOfFirstBracket = rawLine.IndexOf('[');
                     int indexOfLastBracket = rawLine.IndexOf(']');
 
+
                     ulong memoryLocation = ulong.Parse(rawLine.Substring(4, indexOfLastBracket - 4));
+
+                    List<ulong> memLocList = createMemLocList(memoryLocation, mask);
 
                     string stringNum = rawLine.Substring(indexOfEqualSign + 2, rawLine.Length - (indexOfEqualSign + 2));
                     ulong lineNumber = ulong.Parse(stringNum);
 
-                    ulong maskedNumber = maskNumber(lineNumber, mask);
+                    // ulong maskedNumber = maskNumber(lineNumber, mask);
 
-                    if (initedmemory.ContainsKey(memoryLocation))
-                        initedmemory[memoryLocation] = maskedNumber;
-                    else
-                        initedmemory.Add(memoryLocation, maskedNumber);
+                    foreach (ulong memLoc in memLocList)
+                    {
+
+                        if (initedmemory.ContainsKey(memLoc))
+                            initedmemory[memLoc] = lineNumber;
+                        else
+                            initedmemory.Add(memLoc, lineNumber);
+                    }
                 }
             }
 
             return initedmemory;
+        }
+
+        private static List<ulong> createMemLocList(ulong memoryLocation, string mask)
+        {
+            List<ulong> returnList = new List<ulong>();
+
+            char[] maskArray = mask.ToCharArray();
+            int countofX = maskArray.Where(c => c == 'X').ToArray().Count();
+
+            List<int> addressOfEveryX = new List<int>();
+
+            for (int i = 0; i < mask.Length; i++)
+            {
+                if (maskArray[i] == 'X')
+                {
+                    addressOfEveryX.Add(i);
+                }
+            }
+
+            List<int> addressOfEvery1 = new List<int>();
+
+            for (int i = 0; i < mask.Length; i++)
+            {
+                if (maskArray[i] == '1')
+                {
+                    addressOfEvery1.Add(i);
+                }
+            }
+
+            for (int j = 0; j < (int)Math.Pow(2, countofX); j++)
+            {
+                string binString = Convert.ToString(j, 2).PadLeft(countofX, '0');
+
+                char[] addressAsBinArray = Convert.ToString((long)memoryLocation, 2).PadLeft(36, '0').ToCharArray();
+
+                // Mask the X
+                for (int k = binString.Length - 1; k >= 0; k--)
+                {
+                    addressAsBinArray[addressOfEveryX[k]] = binString[k];
+                }
+
+                // Mask the 1
+                for (int l = 0; l < addressOfEvery1.Count(); l++)
+                {
+                    addressAsBinArray[addressOfEvery1[l]] = '1';
+                }
+
+                ulong newlyMaskedAddress = Convert.ToUInt64(string.Join("", addressAsBinArray), 2);
+
+                returnList.Add(newlyMaskedAddress);
+            }
+
+            return returnList;
+        }
+
+        private static int countLoopsInMask(string mask)
+        {
+            char[] maskArray = mask.ToCharArray();
+
+            char[] xArray = maskArray.Where(c => c == 'X').ToArray();
+
+            int countOfX = xArray.Count();
+
+            return (int)Math.Pow(2, countOfX);
         }
 
         private static ulong maskNumber(ulong lineNumber, string mask)
