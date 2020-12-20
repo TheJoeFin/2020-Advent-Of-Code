c55df764419f12d673526a2debb49119dad91474
Joseph Finney <josephfinney@LIVE.COM>
2020-12-19 20:21:37 -0600

    Initial work on day 16

--- Code Files/Day16.cs
+++ Code Files/Day16.cs
@@ -7,9 +7,30 @@ using System.Reflection;
 
 namespace _2020_Advent_Of_Code
 {
+    public class TicketInformation
+    {
+        public List<int> MyTicket { get; set; }
+
+        public List<List<int>> NearbyTickets { get; set; }
+
+        public int Rules { get; set; }
+
+    }
+
     public static class Day16
     {
-        public const string TestString = @"";
+        public const string TestString = @"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12";
 
         private const string dayNumber = "16";
 
@@ -18,6 +39,8 @@ namespace _2020_Advent_Of_Code
             Console.WriteLine($"Running Day {dayNumber} {DateTime.Now.ToShortTimeString()}");
 
             string inputString = File.ReadAllText($"Input Files/Day{dayNumber}Input.txt");
+
+
         }
     }
 }
